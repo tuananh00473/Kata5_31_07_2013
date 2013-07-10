84bde34e4f6328027a462ac7c7e02da0addcc616
unknown <anhnt@58PHP-NAMNT-PC.qsoft.com.vn>
2013-07-10 14:04:12 +0700

    BankAccount Kata3 Round2 - started 10.07.2013

---
+++ BankAccountKata3_2/src/main/java/com/qsoft/bak/MainApp.java
@@ -0,0 +1,15 @@
+package com.qsoft.bak;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: anhnt
+ * Date: 7/10/13
+ * Time: 1:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainApp
+{
+    public static void main(String[] args)
+    {
+    }
+}
---
+++ BankAccountKata3_2/src/main/java/com/qsoft/bak/persistence/model/TransactionDTO.java
@@ -0,0 +1,12 @@
+package com.qsoft.bak.persistence.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: anhnt
+ * Date: 7/10/13
+ * Time: 2:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TransactionDTO
+{
+}
